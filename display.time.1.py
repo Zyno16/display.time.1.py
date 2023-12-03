1bf4cdfe80ef0618a883730fc3c5fb00b85a3e41
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-12-03 16:07:57 +0000

    Add files via upload

---
+++ display.time.1.py
@@ -0,0 +1,15 @@
+import datetime
+
+now = datetime.datetime.now()
+d = now.day #display the day
+m = now.month #display month
+y = now.year#display the year
+h = now.hour#display hour
+m =now.minute#display minute
+s =now.second #display second
+ms =now.microsecond#display micro second
+#print(d+"/"+m+"/"+y)
+#print(h)
+#print(m)
+#print(s)
+#print(ms)
